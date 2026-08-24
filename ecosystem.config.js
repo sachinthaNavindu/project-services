8442c9198789e4add92cc5e1e96030425851f4f5
sachinthaNavindu <navindus485@gmail.com>
2026-08-24 11:57:05 +0530

    Updated submodules

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   apps: [
     {
+      name: "cloud-sql-auth-proxy",
+      script: " ./cloud-sql-proxy project-61a10a75-5675-4d5b-a2d:asia-southeast1:mysql-vm project-61a10a75-5675-4d5b-a2d:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-proxy.log"
+    },
+    {
       name: "service-student",
       script: "java",
       args: "-jar service-student/target/Student-Service-1.0.0.jar",
